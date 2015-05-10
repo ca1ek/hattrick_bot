2eb66713ea2d6aacde9142ca698ef4da11c90356
ca1ek <ca1ekowy@gmail.com>
2015-05-10 13:51:04 +0200

    Opens up Firefox, goes to the website, types in sample text into the form

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-__author__ = 'calek'
+__author__ = 'ca1ek'
+
+import selenium
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://hattrick.org')
+
+login = browser.find_element_by_xpath('.//*[@id=\'txtUserName\']')
+password = browser.find_element_by_xpath('.//*[@id=\'txtPassword\']')
+
+login.send_keys("login")
+password.send_keys("password")

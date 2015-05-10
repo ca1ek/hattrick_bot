935195dd639cfabb00369c582bc6c70a821d21b7
ca1ek <ca1ekowy@gmail.com>
2015-05-10 15:27:43 +0200

    Bot goes to adding to pool, then quits the browser

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 __author__ = 'ca1ek'
 
-import selenium
 from selenium import webdriver
 
 browser = webdriver.Firefox()
@@ -21,6 +20,16 @@ button.click()
 my_club = browser.find_element_by_xpath('.//*[@id=\'myClubLink\']')
 my_club.click()
 # now on the my club page
-challenges = browser.find_element_by_partial_link_text('Wyzwania')
-challenges.click()
+
+while True:
+    try:
+        challenges = browser.find_element_by_partial_link_text('Wyzwania')
+        challenges.click()
+    except NoSuchElementException:
+        print("it works just half the time for some reason, trying again")
 # now on the challenges page
+add_to_pool = browser.find_element_by_xpath('.//*[@id=\'ctl00_ctl00_CPContent_CPSidebar_btnShowPoolSettings\']')
+add_to_pool.click()
+
+browser.quit()
+

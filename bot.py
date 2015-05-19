0e05f70325c6702f78b7d27945d0e19cf4c9b648
ca1ek <ca1ek@users.noreply.github.com>
2015-05-19 19:30:14 +0200

    Using chromium as firefox sucks ballz

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from time import sleep
 import random
 
 try:
-    browser = webdriver.Firefox()
+    browser = webdriver.Chrome()
     browser.get('http://hattrick.org/Startpage2.aspx')
     browser.set_window_size(1280, 720)
 
@@ -69,31 +69,52 @@ try:
             print("Matches link not found for some reason, retrying")
     # now on matches page
     while True:
-        try: # trying to find a match
-            match = browser.find_element_by_xpath(
+        try:  # trying to find a match
+            match = browser.find_elements_by_xpath(
                 './/*[@id=\'mainBody\']/table/tbody/tr[td[2]/img[contains(@class, \'matchFriendly\')]]/td[7]/a/img')
-            match.click()
-            break
+            for x in range(len(match)):
+                if 'matchOrderWrite' in match[x].get_attribute('class'):
+                    match[x].click()
+                    print('clicky')
+                    clicked = True
+                    break
+                else:
+                    print('notclicky')
+            if clicked:
+                break
+
         except selenium.common.exceptions.NoSuchElementException:
             print("no match found yet, retrying in about 11-13 mintues")
             sleep(600 + random.randint(60, 180))
+        print("prehaps loopz?")
 
     # now on the match selection stuffs page
+    print("notclickyagain")
     button = browser.find_element_by_xpath('.//*[@id=\'lineups\']')
     button.click()
+    print("clickyagain")
     # lineups menu is now open
+    print("linepzopen")
     button = browser.find_element_by_xpath('.//*[@id=\'ui-id-5\']/span')
     button.click()
+    sleep(5)
+    print("linepz")
     # now in saved lineups selection
+    print("linepzselect")
     button = browser.find_element_by_xpath('.//*[@id=\'savedlineup_8240059\']')
     button.click()
+    sleep(5)
+    print("linepz")
     # saved lineup now select
+    print("linepzsent")
     button = browser.find_element_by_xpath('.//*[@id=\'send\']')
     button.click()
+    sleep(5)
+    print("linepz")
     # lineup now set
 
 
     browser.quit()
 except:
-    pass
+    raise
     browser.quit()
